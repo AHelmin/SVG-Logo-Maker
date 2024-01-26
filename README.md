d6337b3bee6d7b476992f89f4e75299f6f05edd7
helm0127 <helm0127@morris.umn.edu>
2024-01-26 16:01:27 -0600

    added examples, added README, removed some console.log from index

---
+++ README.md
@@ -0,0 +1,59 @@
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+# SVG Logo Maker
+
+## Description
+
+This project was created so that a logo that represents your company can be created by an SVG file.
+
+## Installation
+
+Installation of Node.js is required to run this module. Additionally, The node packages inquirer version 8.2.4 and Jest must be installed.
+
+## Usage
+
+To start using the SVG Logo Maker, enter the command 'node index.js' into the Terminal. 
+
+![How To Start Logo Maker](./assets/images/Start_svg_logo_maker.png)
+
+Then just answer the question prompts appropriately as they appear.
+
+![Logo Maker Question Example](./assets/images/SVG_logo_maker_questions.png)
+
+To select a shape, use the arrow keys to select the desired shape and press enter.
+
+![Shape List](./assets/images/SVG_shape_list.png)
+
+To test this application, make sure that the node package Jest is installed and enter 'npm test' into the Terminal.
+
+![How To Run Test](./assets/images/SVG_logo_maker_test.png)
+
+The resulting SVG logo can be found in the main directory. Additionally, a video demonstration can be found at this link: https://drive.google.com/file/d/1BfmhebE9e_3_fVi7IJRnaCT241hS2CC-/view.
+
+## Credits
+
+Thanks goes to the University of Minnesota and Edx Bootcamp for providing the assistance needed to complete this project. 
+
+## License
+
+MIT License
+
+Copyright (c) 2024 helm0127
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -5,36 +5,34 @@ const Circle = require("./lib/Circle")
 const Triangle = require("./lib/Triangle")
 const Logo = require("./lib/Logo")
 
-
+//variable to store user prompt questions
 const questions = [
   {
-      type: 'input',
-      message: 'Please enter your company letters(up to 3).',
-      name: 'letters',
+    type: 'input',
+    message: 'Please enter your company letters(up to 3).',
+    name: 'letters',
+    validate: input => input.length <= 3,
   },
   {
     type: 'input',
     message: 'Please enter the letter color(use color names or hexadecimal color notation).',
     name: 'letterColor',
-},
-{
-  type: 'list',
-  message: 'Which logo shape would you like?',
-  name: 'shape',
-  choices: ['square', 'circle', 'triangle']
-},
-{
-  type: 'input',
-  message: 'Please enter the background color(use color names or hexadecimal color notation).',
-  name: 'shapeColor',
-}]
+  },
+  {
+    type: 'list',
+    message: 'Which logo shape would you like?',
+    name: 'shape',
+    choices: ['square', 'circle', 'triangle']
+  },
+  {
+    type: 'input',
+    message: 'Please enter the background color(use color names or hexadecimal color notation).',
+    name: 'shapeColor',
+  }]
 
+//function grabs the user input and sends the data to the correct shape function
 async function logoInformation() {
   const logoInfo = await inquirer.prompt(questions);
-  if (logoInfo.letters.length > 3) {
-    console.log('Please enter 3 or less letters.');
-    logoInformation();
-  }
   const newLogo = new Logo(logoInfo.shape, logoInfo.shapeColor, logoInfo.letterColor, logoInfo.letters);
   if (newLogo.shape === 'circle') {
     newCircle(newLogo)
@@ -45,22 +43,23 @@ async function logoInformation() {
   if (newLogo.shape === 'triangle') {
     newTriangle(newLogo)
   }
-  console.log(newLogo)
 };
 
+//if circle, then new circle object created and method is invoked to create svg text, then sent to write 
 function newCircle(newLogo) {
   const newCircle = new Circle(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
   const logo = newCircle.render();
-  console.log(logo)
   writeToFile('Logo.svg', logo);
 };
 
+//if square, then new square object created and method is invoked to create svg text, then sent to write 
 function newSquare(newLogo) {
   const newSquare = new Square(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
   const logo = newSquare.render();
   writeToFile('Logo.svg', logo);
 };
 
+//if triangle, then new triangle object created and method is invoked to create svg text, then sent to write 
 function newTriangle(newLogo) {
   const newTriangle = new Triangle(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
   const logo = newTriangle.render();
@@ -68,9 +67,9 @@ function newTriangle(newLogo) {
 };
 
 //function writes SVG file
-function writeToFile(fileName, data) { 
+function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
-      err ? console.error(err) : console.log('Success!')
+    err ? console.error(err) : console.log('Success!')
   )
 };
 
