56d5bf43d4e8b542ffa18160f148230cc0581f05
helm0127 <helm0127@morris.umn.edu>
2024-01-25 20:09:58 -0600

    added functionality to index.js, added methods to shape classes

--- index.js
+++ index.js
@@ -2,12 +2,81 @@ const inquirer = require("inquirer")
 const Square = require("./lib/Square")
 const Circle = require("./lib/Circle")
 const Triangle = require("./lib/Triangle")
-const Shapes = require("./lib/Shapes")
+const Logo = require("./lib/Logo")
 
 
 const questions = [
   {
       type: 'input',
-      message: 'Please enter the title of your project.',
-      name: 'title',
+      message: 'Please enter your company letters(up to 3).',
+      name: 'letters',
   },
+  {
+    type: 'input',
+    message: 'Please enter the letter color(use color names or hexadecimal color notation).',
+    name: 'letterColor',
+},
+{
+  type: 'list',
+  message: 'Which logo shape would you like?',
+  name: 'shape',
+  choices: ['square', 'circle', 'triangle']
+},
+{
+  type: 'input',
+  message: 'Please enter the background color(use color names or hexadecimal color notation).',
+  name: 'shapeColor',
+}]
+
+async function logoInformation() {
+  const logoInfo = await inquirer.prompt(questions);
+  if (logoInfo.letters.length > 3) {
+    console.log('Please enter 3 or less letters.');
+    logoInformation();
+  }
+  const newLogo = new Logo(logoInfo.shape, logoInfo.shapeColor, logoInfo.letterColor, logoInfo.letters);
+  if (newLogo.shape === 'circle') {
+    newCircle(newLogo)
+  }
+  if (newLogo.shape === 'square') {
+    newSquare(newLogo)
+  }
+  if (newLogo.shape === 'triangle') {
+    newTriangle(newLogo)
+  }
+  console.log(newLogo)
+};
+
+function newCircle(newLogo) {
+  const newCircle = new Circle(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
+  const logo = newCircle.render();
+  writeToFile('Logo.SVG', logo);
+};
+
+function newSquare(newLogo) {
+  const newSquare = new Square(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
+  const logo = newSquare.render();
+  writeToFile('Logo.SVG', logo);
+};
+
+function newTriangle(newLogo) {
+  const newTriangle = new Triangle(newLogo.shape, newLogo.shapeColor, newLogo.letterColor, newLogo.letters);
+  const logo = newTriangle.render();
+  writeToFile('Logo.SVG', logo);
+};
+
+//function writes SVG file
+function writeToFile(fileName, data) { 
+  fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('Success!')
+  )
+};
+
+//function initializes app
+function init() {
+  logoInformation();
+};
+
+// Function call initializes app
+init();
+
